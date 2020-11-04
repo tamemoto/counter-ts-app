95d1cd6de9ea0364816fa0d0f83907932523f293
tamemotoyusuke <tamemotoyou@gmail.com>
2020-11-04 14:30:36 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,36 @@
 // eslint-disable-line no-unused-vars
-import React, { Component, ReactElement } from 'react';
+import React, { FC, useState } from 'react';
 import { Button, Card, Statistic } from 'semantic-ui-react';
 import './App.css';
 
-type State = {
-  count: number;
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+  const reset = () => setCount(0);
+  const increment = () => setCount((c) => c + 1);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>カウンター</h1>
+      </header>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>count</Statistic.Label>
+          <Statistic.Value>{count}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <div className="ui two buttons">
+            <Button color="red" onClick={reset}>
+              Reset
+            </Button>
+            <Button color="red" onClick={increment}>
+              +1
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    </div>
+  );
 };
-class App extends Component<unknown, State> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = { count: 0 };
-  }
-
-  reset(): void {
-    this.setState({ count: 0 });
-  }
-
-  increment(): void {
-    this.setState((state) => ({ count: state.count + 1 }));
-  }
-
-  render(): ReactElement {
-    const { count } = this.state;
-
-    return (
-      <div className="container">
-        <header>
-          <h1>カウンター</h1>
-        </header>
-        <Card>
-          <Statistic className="number-board">
-            <Statistic.Label>count</Statistic.Label>
-            <Statistic.Value>{count}</Statistic.Value>
-          </Statistic>
-          <Card.Content>
-            <div className="ui two buttons">
-              <Button color="red" onClick={() => this.reset()}>
-                Reset
-              </Button>
-              <Button color="red" onClick={() => this.increment()}>
-                count
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
 
 export default App;
